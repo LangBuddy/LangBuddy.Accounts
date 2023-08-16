88c3925bd72f613ed3e0a054e3a53b4e92b143bf
FremyXS <antonovch13@mail.ru>
2023-08-16 22:50:30 +0300

    feat(Database): add DependencyInjection

---
+++ LangBuddy.Accounts.Database/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LangBuddy.Accounts.Database
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            //"Server=localhost;Port=6789;Database=MiniChatDataBase;Username=postgres;Password=123456"
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AccountsDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString,
+                    options => options.UseNodaTime());
+            });
+
+            services.AddScoped<AccountsDbContext>();
+
+            return services;
+        }
+    }
+}
