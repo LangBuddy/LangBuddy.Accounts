f639e8883041cd504cce8babec0402d1041a779e
FremyXS <antonovch13@mail.ru>
2023-08-16 22:30:15 +0300

    feat(Database): add entity Account

---
+++ LangBuddy.Accounts.Database/Entity/Account.cs
@@ -0,0 +1,17 @@
+﻿using LangBuddy.Accounts.Database.Entity.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Accounts.Database.Entity
+{
+    public class Account: EntityBase
+    {
+        public string Email { get; set; }
+        public string Nickname { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+    }
+}
