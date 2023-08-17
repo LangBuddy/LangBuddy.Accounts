3f7ae815d7f72705111f6671ad6339a750a85232
FremyXS <antonovch13@mail.ru>
2023-08-17 16:15:13 +0300

    feat(Service): add command CheckingIdCommand

---
+++ LangBuddy.Accounts.Service/Account/Commands/CheckingIdCommand.cs
@@ -0,0 +1,25 @@
+﻿using LangBuddy.Accounts.Service.Account.Common;
+
+namespace LangBuddy.Accounts.Service.Account.Commands
+{
+    public class CheckingIdCommand: ICheckingIdCommand
+    {
+        private readonly IGetAccountByIdCommand _getAccountByIdCommand;
+        
+        public CheckingIdCommand(IGetAccountByIdCommand getAccountByIdCommand)
+        {
+            _getAccountByIdCommand = getAccountByIdCommand;
+        }
+        public async Task<Database.Entity.Account> Invoke(long id)
+        {
+            var account = await _getAccountByIdCommand.Invoke(id);
+
+            if (account == null)
+            {
+                throw new ArgumentNullException("The account was not found");
+            }
+
+            return account;
+        }
+    }
+}
---
+++ LangBuddy.Accounts.Service/Account/Common/ICheckingIdCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace LangBuddy.Accounts.Service.Account.Common
+{
+    public interface ICheckingIdCommand
+    {
+        Task<Database.Entity.Account> Invoke(long id);
+    }
+}
