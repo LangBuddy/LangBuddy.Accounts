baea6ffaa4e7630c684db1081cf182616ed48aad
FremyXS <antonovch13@mail.ru>
2023-08-17 16:58:04 +0300

    feat(Service): add service AccountService

---
+++ LangBuddy.Accounts.Service/Account/AccountService.cs
@@ -0,0 +1,43 @@
+﻿using LangBuddy.Accounts.Models.Request;
+using LangBuddy.Accounts.Service.Account.Common;
+
+namespace LangBuddy.Accounts.Service.Account
+{
+    public class AccountService : IAccountService
+    {
+        private readonly ICreateAccountCommand _createAccountCommand;
+        private readonly IDeleteAccountByIdCommand _deleteAccountByIdCommand;
+        private readonly IUpdateAccountCommand _updateAccountCommand;
+        private readonly IGetAllAccountsCommand _getAllAccountsCommand;
+        public AccountService(ICreateAccountCommand createAccountCommand,
+            IDeleteAccountByIdCommand deleteAccountByIdCommand,
+            IUpdateAccountCommand updateAccountCommand,
+            IGetAllAccountsCommand getAllAccountsCommand) 
+        {
+            _createAccountCommand = createAccountCommand;
+            _deleteAccountByIdCommand = deleteAccountByIdCommand;
+            _updateAccountCommand = updateAccountCommand;
+            _getAllAccountsCommand = getAllAccountsCommand;
+        }
+
+        public async Task<List<Database.Entity.Account>> GetAll()
+        {
+            return await _getAllAccountsCommand.Invoke();
+        }
+
+        public async Task<int> Create(AccountCreateRequest accountCreateRequest)
+        {
+            return await _createAccountCommand.Invoke(accountCreateRequest);
+        }
+
+        public async Task<int> Delete(long id)
+        {
+            return await _deleteAccountByIdCommand.Invoke(id);
+        }
+
+        public async Task<int> Update(long id, AccountUpdateRequest accountUpdateRequest)
+        {
+            return await _updateAccountCommand.Invoke(id, accountUpdateRequest);
+        }
+    }
+}
---
+++ LangBuddy.Accounts.Service/Account/Common/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using LangBuddy.Accounts.Models.Request;
+
+namespace LangBuddy.Accounts.Service.Account.Common
+{
+    public interface IAccountService
+    {
+        Task<List<Database.Entity.Account>> GetAll();
+
+        Task<int> Create(AccountCreateRequest accountCreateRequest);
+
+        Task<int> Delete(long id);
+
+        Task<int> Update(long id, AccountUpdateRequest accountUpdateRequest);
+    }
+}
