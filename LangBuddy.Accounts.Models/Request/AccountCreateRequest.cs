93bb553dbba79d82e07eef0c1abfcff3909a6813
FremyXS <antonovch13@mail.ru>
2023-08-17 14:05:24 +0300

    feat(Models): add request AccountCreateRequest

---
+++ LangBuddy.Accounts.Models/Request/AccountCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Accounts.Models.Request
+{
+    public record AccountCreateRequest(
+        string Email, string Nickname, byte[] PasswordSalt, byte[] PasswordHash
+    );
+}
