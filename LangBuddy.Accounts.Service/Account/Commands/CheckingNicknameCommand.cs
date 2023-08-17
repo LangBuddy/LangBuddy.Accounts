3f7a1e396a3e524693a309d57afc641637d64a6a
FremyXS <antonovch13@mail.ru>
2023-08-17 15:45:25 +0300

    feat(Service): add command CheckingNicknameCommand

---
+++ LangBuddy.Accounts.Service/Account/Commands/CheckingNicknameCommand.cs
@@ -0,0 +1,24 @@
+﻿using LangBuddy.Accounts.Service.Account.Common;
+
+namespace LangBuddy.Accounts.Service.Account.Commands
+{
+    public class CheckingNicknameCommand: ICheckingNicknameCommand
+    {
+        private readonly IGetAccountByNicknameCommand _getAccountByNicknameCommand;
+
+        public CheckingNicknameCommand(IGetAccountByNicknameCommand getAccountByNicknameCommand)
+        {
+            _getAccountByNicknameCommand = getAccountByNicknameCommand;
+        }
+
+        public async Task Invoke(string nickname)
+        {
+            var accountByNickname = await _getAccountByNicknameCommand.Invoke(nickname);
+
+            if (accountByNickname != null)
+            {
+                throw new ArgumentException("Nickname is already in use");
+            }
+        }
+    }
+}
