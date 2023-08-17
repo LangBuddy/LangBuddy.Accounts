0670e7f7700c61859616537e14c606b69fdf7a24
FremyXS <antonovch13@mail.ru>
2023-08-17 14:13:19 +0300

    feat(Models): add mapper AccountMapper

---
+++ LangBuddy.Accounts.Models/Mappers/AccountMapper.cs
@@ -0,0 +1,18 @@
+﻿using LangBuddy.Accounts.Database.Entity;
+using LangBuddy.Accounts.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Accounts.Models.Mappers
+{
+    public class AccountMapper
+    {
+        public static Account ToModel(this AccountCreateRequest accountCreateRequest)
+        {
+            return accountCreateRequest.Adapt<Account>();
+        }
+    }
+}
