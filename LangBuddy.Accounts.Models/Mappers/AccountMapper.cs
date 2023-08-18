6e08f75f3eea51eb462459b6f1b51297c0e6788e
FremyXS <antonovch13@mail.ru>
2023-08-18 20:44:45 +0300

    refactor(Models): changes AccountMapper, add new mapper ToAccountPasswordHashDto

--- LangBuddy.Accounts.Models/Mappers/AccountMapper.cs
+++ LangBuddy.Accounts.Models/Mappers/AccountMapper.cs
@@ -1,4 +1,5 @@
 ﻿using LangBuddy.Accounts.Database.Entity;
+using LangBuddy.Accounts.Models.Dto;
 using LangBuddy.Accounts.Models.Request;
 using Mapster;
 
@@ -9,6 +10,11 @@ namespace LangBuddy.Accounts.Models.Mappers
         public static Account ToModel(this AccountCreateRequest accountCreateRequest)
         {
             return accountCreateRequest.Adapt<Account>();
+        }
+
+        public static AccountPasswordHashDto ToAccountPasswordHashDto(this Account account)
+        {
+            return account.Adapt<AccountPasswordHashDto>();
         }
     }
 }
