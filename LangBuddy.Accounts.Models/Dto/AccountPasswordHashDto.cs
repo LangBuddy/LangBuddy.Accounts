8426097e399647ea01b4d148c09dbff7a1e77d32
FremyXS <antonovch13@mail.ru>
2023-08-18 20:43:55 +0300

    feat(Models): add dto AccountPasswordHashDto

---
+++ LangBuddy.Accounts.Models/Dto/AccountPasswordHashDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangBuddy.Accounts.Models.Dto
+{
+    public record AccountPasswordHashDto(
+        byte[] PasswordSalt, byte[] PasswordHash
+    );
+}
