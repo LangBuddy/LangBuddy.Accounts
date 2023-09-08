9fa4924c01a7a50f5b3de3043c5affa045d67175
FremyXS <antonovch13@mail.ru>
2023-09-08 21:41:30 +0300

    refactor(Models): remove other imports

--- LangBuddy.Accounts.Models/Dto/AccountPasswordHashDto.cs
+++ LangBuddy.Accounts.Models/Dto/AccountPasswordHashDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LangBuddy.Accounts.Models.Dto
+﻿namespace LangBuddy.Accounts.Models.Dto
 {
     public record AccountPasswordHashDto(
         byte[] PasswordSalt, byte[] PasswordHash
