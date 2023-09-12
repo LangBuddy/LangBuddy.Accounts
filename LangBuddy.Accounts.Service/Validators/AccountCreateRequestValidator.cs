c06b39fda1639c035f0c1365385eb51b1bb33023
FremyXS <antonovch13@mail.ru>
2023-09-12 13:38:33 +0300

    feat(Service): add validator AccountCreateRequestValidator

---
+++ LangBuddy.Accounts.Service/Validators/AccountCreateRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using LangBuddy.Accounts.Models.Request;
+
+namespace LangBuddy.Accounts.Service.Validators
+{
+    public class AccountCreateRequestValidator: AbstractValidator<AccountCreateRequest>
+    {
+        public AccountCreateRequestValidator() 
+        {
+            RuleFor(el => el).NotNull().NotEmpty();
+            RuleFor(el => el.Email).NotNull().NotEmpty().EmailAddress();
+            RuleFor(el => el.Nickname).NotNull().NotEmpty();
+            RuleFor(el => el.PasswordHash).NotNull().NotEmpty();
+            RuleFor(el => el.PasswordSalt).NotNull().NotEmpty();
+        }
+    }
+}
