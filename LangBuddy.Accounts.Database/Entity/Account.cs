6695ba671fbb30b9dee6e5660911f25b4cbe5111
FremyXS <antonovch13@mail.ru>
2023-09-12 13:37:43 +0300

    refactor(Database): remove other imports

--- LangBuddy.Accounts.Database/Entity/Account.cs
+++ LangBuddy.Accounts.Database/Entity/Account.cs
@@ -1,9 +1,4 @@
 ﻿using LangBuddy.Accounts.Database.Entity.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LangBuddy.Accounts.Database.Entity
 {
--- LangBuddy.Accounts.Database/Entity/Common/EntityBase.cs
+++ LangBuddy.Accounts.Database/Entity/Common/EntityBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LangBuddy.Accounts.Database.Entity.Common
+﻿namespace LangBuddy.Accounts.Database.Entity.Common
 {
     public abstract class EntityBase
     {
