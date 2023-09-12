0add78490aab119c424e1614f48ec05e47623321
FremyXS <antonovch13@mail.ru>
2023-09-12 14:20:32 +0300

    refactor(Web): remove dependency to Database

--- LangBuddy.Accounts.Web/Program.cs
+++ LangBuddy.Accounts.Web/Program.cs
@@ -1,4 +1,3 @@
-using LangBuddy.Accounts.Database;
 using LangBuddy.Accounts.Service;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +10,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
